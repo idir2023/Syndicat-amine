6202ab3d17c095175e254f1d3a120f599904149b
Aichanjimate2022 <NjimateAicha>
2024-10-11 17:32:05 +0100

    fixe all

---
+++ .history/app/Http/Controllers/UserController_20241011161437.php
@@ -0,0 +1,279 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Residence;
+use App\Models\Invitation;
+use Illuminate\Support\Str;
+use App\Mail\InvitationMail;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Auth\Events\Registered;
+use App\Models\ChMessage as Message;
+
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        $token = $request->query('token');
+
+        // Redirect if token is missing or invalid
+        if (!$token) {
+            return redirect()->route('formRegister')->withErrors('Invalid or missing token.');
+        }
+
+        $invitation = Invitation::where('token', $token)->first();
+
+        if (!$invitation) {
+            return redirect()->route('formRegister')->withErrors('Invalid or expired token.');
+        }
+
+        if ($invitation->expires_at && $invitation->expires_at->isPast()) {
+            return redirect()->route('formRegister')->withErrors('This invitation has expired.');
+        }
+
+        // Check if the role is either 'superadmin' or 'admin'
+        if (in_array($invitation->role, ['superadmin'])) {
+            return view('emails.register.register', [
+                'token' => $token,
+                'email' => $invitation->email,
+                'role' => $invitation->role
+            ]);
+        }
+        $residence = Residence::findOrFail($invitation->residence_id);
+        // dd($residence);
+
+        return view('emails.register.register', [
+            'token' => $token,
+            'email' => $invitation->email,
+            'role' => $invitation->role,
+            'residence' => $residence
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        $token = $request->query('token');
+
+        if (!$token || !$invitation = Invitation::where('token', $token)->first()) {
+            return redirect()->route('home')->withErrors('Invalid or missing token.');
+        }
+
+        return view('auth.register', [
+            'token' => $token,
+            'email' => $invitation->email,
+            'role' => $invitation->role,
+            'residence_id' => $invitation->residence_id,
+        ]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'prenom' => 'required|string|max:255',
+            'nom' => 'required|string|max:255',
+            'telephone' => 'required|string|max:20',
+            'immeuble' => 'required|string|max:255',
+            'appartement' => 'required|string|max:255',
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'email' => 'required|email|exists:invitations,email',
+            'role' => 'required|string|exists:roles,name',
+            'residence_id' => 'nullable|exists:residences,id',
+            'token' => 'required|string|exists:invitations,token',
+        ]);
+
+        $user = User::create([
+            'prenom' => $request->prenom,
+            'name' => $request->nom,
+            'phone' => $request->telephone,
+            'Num_Immenble' => $request->immeuble,
+            'Num_Appartement' => $request->appartement,
+            'password' => Hash::make($request->password),
+            'email' => $request->email,
+            'residence_id' => $request->residence_id, // Store the residence ID
+        ]);
+
+        $user->assignRole($request->role);
+
+        event(new Registered($user));
+
+        // Delete the invitation after user registration
+        Invitation::where('token', $request->token)->delete();
+
+        Auth::login($user);
+
+        return redirect(route('dashboard.index'))->with('status', 'Registration successful!');
+    }
+
+
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function searchContacts(Request $request)
+    {
+        // Get the search input, or default to an empty string
+        $search = $request->search ?? '';
+        // Query for users involved in conversations
+        $users = Message::join('users', function ($join) {
+                $join->on('ch_messages.from_id', '=', 'users.id')
+                     ->orOn('ch_messages.to_id', '=', 'users.id');
+            })
+            ->leftjoin('residences', 'users.residence_id', '=', 'residences.id')
+            ->where('users.residence_id', Auth::user()->residence_id) 
+            // ->where(function ($query) {
+            //     $query->where('ch_messages.from_id', Auth::id())
+            //           ->orWhere('ch_messages.to_id', Auth::id());
+            // })
+            ->where('users.name', 'like', '%' . $search . '%') // Search by user name
+            ->where('users.id', '!=', Auth::id()) // Exclude the current user
+            ->distinct('users.id') // Ensure unique users
+            ->select('users.*') // Select user details
+            ->paginate(10); // Optionally paginate results
+    
+        // Return the view with users
+        return view('tchat.show', compact('users'));
+    }
+/*     public function getGroup(Request $request)
+    {
+        // Get the search input, or default to an empty string
+        $search = $request->search ?? '';
+        // Query for users involved in conversations
+        $users = Message::join('users', function ($join) {
+                $join->on('ch_messages.from_id', '=', 'users.id')
+                     ->orOn('ch_messages.to_id', '=', 'users.id');
+            })
+            ->leftjoin('residences', 'users.residence_id', '=', 'residences.id')
+            ->where('users.residence_id', Auth::user()->residence_id) 
+            // ->where(function ($query) {
+            //     $query->where('ch_messages.from_id', Auth::id())
+            //           ->orWhere('ch_messages.to_id', Auth::id());
+            // })
+            ->where('users.name', 'like', '%' . $search . '%') // Search by user name
+            ->where('users.id', '!=', Auth::id()) // Exclude the current user
+            ->distinct('users.id') // Ensure unique users
+            ->select('users.*') // Select user details
+            ->paginate(10); // Optionally paginate results
+    
+        // Return the view with users
+        return view('tchat.show_group', compact('users'));
+    }
+
+
+
+    
+ */
+
+ public function getGroup(Request $request)
+{
+    // Get the search input, or default to an empty string
+
+    function getCurrentUrl() {
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+        $host = $_SERVER['HTTP_HOST'];
+        $requestUri = $_SERVER['REQUEST_URI'];
+        
+        return $protocol . $host . $requestUri;
+    }
+
+    $url = getCurrentUrl();
+
+
+
+    $search = $request->search ?? '';
+
+    // Get the residence ID from the URL if it exists
+    $residenceId = null;
+    
+    // Check if there is a query string and if it starts with a number
+    if ($request->query() && preg_match('/^\d+$/', $request->getQueryString())) {
+        
+        $residenceId = intval($request->getQueryString()); // Convert to integer
+        
+    } else {
+        $residenceId = Auth::user()->residence_id; // Fallback to the authenticated user's residence ID
+    }
+
+    
+
+    // Query for users who belong to the specified residence
+    $users = User::leftJoin('residences', 'users.residence_id', '=', 'residences.id')
+            ->where('users.residence_id', $residenceId) // Use the residence ID from the URL or authenticated user
+            ->where('users.name', 'like', '%' . $search . '%') // Search by user name
+            ->where('users.id', '!=', Auth::id()) // Exclude the current user
+            ->distinct('users.id') // Ensure unique users
+            ->select('users.*') // Select user details
+            ->paginate(10); // Optionally paginate results
+
+    // Return the view with users
+    return view('tchat.show_group', compact('users'));
+}
+
+    public function update(Request $request, string $id)
+    {
+        $user = Auth::user();
+        // dd($request->all());
+
+        $residence = Residence::findOrFail($id);
+
+        // Validate the form data
+        $request->validate([
+            'profile_image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048', // adjust the max size as needed
+            'phone' => 'nullable|string|max:255',
+            'password' => 'nullable|string|min:6',
+        ]);
+
+        if ($request->hasFile('profile_image')) {
+            $imagePath = $request->file('profile_image')->store('profile_images', 'public');
+            $user->image = $imagePath;
+            // dd($imagePath);
+        }
+        // if ($request->hasFile('profile_image')) {
+        //     $file = $request->file('profile_image');
+        //     dd($file->getClientOriginalName(), $file->getSize(), $file->getMimeType());
+        // }
+
+        if ($request->filled('phone')) {
+            $user->phone = $request->phone;
+        }
+
+        if ($request->filled('password')) {
+            $user->password = Hash::make($request->password);
+        }
+
+        $user->save();
+
+        return redirect()->route('reglages.show', $residence)->with('status', 'Profile updated successfully!');
+    }
+
+
+    public function destroy(string $id)
+    {
+        $user = User::findOrFail($id);
+        $residence = Residence::findOrFail($user->residence_id);
+        $user->roles()->detach();
+        $user->delete();
+
+        // Optionally, you can also delete associated invitations or other related records
+        Invitation::where('email', $user->email)->delete();
+
+        // Redirect back with a success message
+        return redirect()->route('reglages.show', $residence)->with('status', 'User deleted successfully!');
+    }
+
+
+    
+}
---
+++ .history/resources/views/vendor/Chatify/pages/app.blade_20241011152520.php
@@ -0,0 +1,135 @@
+@extends('layouts.main')
+
+@section('content')
+    {{-- Barre de navigation --}}
+    <x-layouts.navbar :user="Auth::user()" route="send.message"></x-layouts.navbar>
+    <script>
+        function changeResidence() {
+            
+           
+        
+        
+        let residenceId = localStorage.getItem('selectedResidenceId');
+            console.log(residenceId)
+        }
+    </script>
+    @include('Chatify::layouts.headLinks')
+
+    <div class="messenger">
+        {{-- Liste des utilisateurs/groupes --}}
+        <div class="messenger-listView {{ !!$id ? 'conversation-active' : '' }}">
+            {{-- En-tête et barre de recherche --}}
+            <div class="m-header">
+                <nav>
+                    <a href="javascript:void(0);">
+                        <span class="messenger-headTitle font-semibold text-[14px] text-[#3C4C7C]">Discussions</span>
+                    </a>
+                    {{-- Boutons de l'en-tête --}}
+                    <nav class="m-header-right">
+                        <a href="{{ route('contacts.search') }}"
+                            class="payPurchase_due rounded-[6px] bg-gradient-to-r from-[#9EAFCE] to-[#697C9B] p-[4px] w-[24px] h-[24px] flex justify-center items-center cursor-pointer">
+                            <img src="{{ asset('assets/images/chat_31.png') }}" class="w-[16px] h-[16px]" alt="Chat">
+                        </a>
+                        <a href="{{ route('chat.residence', ['id' => $residenceId]) }}" class="getGroup rounded-[6px] bg-gradient-to-r from-[#9EAFCE] to-[#697C9B] p-[4px] w-[24px] h-[24px] flex justify-center items-center cursor-pointer">
+                            <img src="{{ asset('assets/images/add_group_1.png') }}" class="w-[16px] h-[16px]" alt="Ajouter un groupe">
+                        </a>
+                    </nav>
+                </nav>
+                {{-- Champ de recherche --}}
+                <input type="text" placeholder="Rechercher" class="messenger-search rounded-[8px] border-[1px_solid_#9EAFCE] m-[0_20px_20px_20px] p-[12.5px_20px] w-[fit-content] text-[12px] text-[#A2A2A2] font-['Inter']">
+            </div>
+
+            {{-- Onglets et listes --}}
+            <div class="m-body contacts-container">
+                {{-- Onglet Utilisateur --}}
+                <div class="show messenger-tab users-tab app-scroll" data-view="users">
+                    {{-- Favoris --}}
+                    <div class="favorites-section">
+                        <p class="messenger-title"><span>Favoris</span></p>
+                        <div class="messenger-favorites app-scroll-hidden"></div>
+                    </div>
+
+                     {{-- groups --}}
+                      <div class="groups-section">
+                        <p class="messenger-title"><span>Groups</span></p>
+                        <div class="messenger-groups app-scroll-hidden"></div>
+                        
+                    </div>
+
+                    {{-- Contacts --}}
+                    <p class="messenger-title"><span>Tous les messages</span></p>
+                    <div class="listOfContacts" style="width: 100%; height: calc(100% - 272px); position: relative;"></div>
+                </div>
+
+                {{-- Onglet Recherche --}}
+                <div class="messenger-tab search-tab app-scroll" data-view="search">
+                    <p class="messenger-title"><span>Recherche</span></p>
+                    <div class="search-records">
+                        <p class="message-hint center-el"><span>Tapez pour rechercher..</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- Zone de messagerie --}}
+        <div class="messenger-messagingView">
+            {{-- titre de l'en-tête et boutons --}}
+            <div class="m-header m-header-messaging">
+                <nav class="chatify-d-flex chatify-justify-content-between chatify-align-items-center">
+                    <div class="chatify-d-flex chatify-justify-content-between chatify-align-items-center">
+                        <a href="#" class="show-listView"><i class="fas fa-arrow-left"></i></a>
+                        <div class="avatar av-s header-avatar" style="margin: 0 10px; margin-top: -5px; margin-bottom: -5px;"></div>
+                        <a href="#" class="user-name">{{ config('chatify.name') }}</a>
+                    </div>
+                    {{-- boutons de l'en-tête --}}
+                    <nav class="m-header-right">
+                        <a href="#" class="add-to-favorite"><i class="fas fa-star"></i></a>
+                        <a href="/"><i class="fas fa-home"></i></a>
+                        <a href="#" class="show-infoSide"><i class="fas fa-info-circle"></i></a>
+                    </nav>
+                </nav>
+                <div class="internet-connection">
+                    <span class="ic-connected">Connecté</span>
+                    <span class="ic-connecting">Connexion en cours...</span>
+                    <span class="ic-noInternet">Pas d'accès à Internet</span>
+                </div>
+            </div>
+
+            <div class="m-body messages-container app-scroll">
+                <div class="messages">
+                    <p class="message-hint center-el"><span>Veuillez sélectionner un chat pour commencer à discuter</span></p>
+                </div>
+                <div class="typing-indicator">
+                    <div class="message-card typing">
+                        <div class="message">
+                            <span class="typing-dots">
+                                <span class="dot dot-1"></span>
+                                <span class="dot dot-2"></span>
+                                <span class="dot dot-3"></span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @include('Chatify::layouts.sendForm')
+        </div>
+    </div>
+
+    {{-- Modal --}}
+    <div class="fixed inset-0 z-50 hidden pay_contact_due_modal bg-black bg-opacity-50 flex items-center justify-center"
+        role="dialog" aria-labelledby="userSearchModalLabel">
+    </div>
+
+    <div class="fixed inset-0 z-50 hidden groupModal bg-black bg-opacity-50 flex items-center justify-center"
+        role="dialog" aria-labelledby="userSearchModalLabel">
+    </div>
+
+    @include('Chatify::layouts.modals')
+    @include('Chatify::layouts.footerLinks')
+    
+@endsection
+<script>
+    function changeResidence(residenceId) {
+        window.location.href = "{{ route('chat.residence', '') }}/" + residenceId;
+    }
+</script>
